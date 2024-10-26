f03d66596d7c3c627910f5648200607b3d43a174
kaneshkha <120207559+kaneshkha@users.noreply.github.com>
2024-10-26 13:19:17 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add interactivity if needed. Example: smooth scrolling for sections
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
